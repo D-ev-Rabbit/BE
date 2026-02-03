306923566223e05902f039140b43b0d0e5b104e2
Lime <agahong1@gmail.com>
2026-02-03 11:16:20 +0900

    feat: first commit

---
+++ src/main/java/com/derabbit/seolstudy/SeolstudyApplication.java
@@ -0,0 +1,13 @@
+package com.derabbit.seolstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeolstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeolstudyApplication.class, args);
+	}
+
+}
