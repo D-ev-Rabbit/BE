700a62592dd597c411ce6a564a54990229699cb3
KMS0514 <comengiee852.mk@gmail.com>
2026-02-09 12:27:35 +0900

    fix: 멘티 과제 다운로드 및 학습 점검 PDF/이미지 표시

--- src/main/java/com/derabbit/seolstudy/domain/todo/dto/response/TodoFileResponse.java
+++ src/main/java/com/derabbit/seolstudy/domain/todo/dto/response/TodoFileResponse.java
@@ -12,15 +12,19 @@ import lombok.Getter;
 public class TodoFileResponse {
 
     private Long fileId;
+    /** 클라이언트용 다운로드 URL: /api/files/{id}/download */
     private String url;
+    private String name;
     private String type;
     private Long version;
     private List<TodoFeedbackResponse> feedbacks;
 
     public static TodoFileResponse from(File file, List<TodoFeedbackResponse> feedbacks) {
+        String downloadUrl = "/api/files/" + file.getId() + "/download";
         return TodoFileResponse.builder()
                 .fileId(file.getId())
-                .url(file.getUrl())
+                .url(downloadUrl)
+                .name(file.getName())
                 .type(file.getType() == null ? null : file.getType().name().toLowerCase())
                 .feedbacks(feedbacks)
                 .build();
